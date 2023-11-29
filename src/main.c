06aa00a89238d864ed415b3319bb7ceb1989d0ec
I-AM-ENGINEER <sdwad6@gmail.com>
2023-11-29 22:20:14 +0300

    first commit

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#define F_CPU	16000000UL
+
+#include <avr/interrupt.h>
+#include <stdlib.h>
+#include <avr/io.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+int main( void ){
+	
+	while (1){
+
+	}
+}
